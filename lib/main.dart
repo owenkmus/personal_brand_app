889255d22efc358c9dcc9c24fcdf67f8f265e8f3
owenkmus <owen_cammus@hotmail.com>
2024-10-14 17:49:51 -0500

    Creacion App Personal

--- lib/main.dart
+++ lib/main.dart
@@ -31,6 +31,7 @@ class SplashScreen extends StatefulWidget {
 
 class SplashScreenState extends State<SplashScreen> {
   late VideoPlayerController _videoController;
+  //bool _showText = false; // Controla la aparición del texto
 
   @override
   void initState() {
@@ -46,6 +47,13 @@ class SplashScreenState extends State<SplashScreen> {
       }).catchError((error) {
         print("Error loading video: $error");
       });
+
+    // Mostrar el texto después de 9 segundos
+    /*Timer(const Duration(seconds: 9), () {
+      setState(() {
+        _showText = true;
+      });
+    });*/
 
     // Redirigir a la pantalla principal después de 10 segundos
     Timer(const Duration(seconds: 10), () {
@@ -77,6 +85,22 @@ class SplashScreenState extends State<SplashScreen> {
               )
             else
               const CircularProgressIndicator(),
+            const SizedBox(height: 20),
+            // Mostrar el texto después de 14 segundos
+            /*if (_showText)
+              const Text(
+                'Innovando soluciones,\nimpulsando negocios.',
+                style: TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.w600, // Peso de la fuente diferente
+                  fontStyle: FontStyle.italic, // Estilo de fuente cursiva
+                  color: Color.fromARGB(255, 180, 19, 19), // Color diferente
+                  fontFamily:
+                      'Roboto', // Tipo de letra diferente (puedes cambiarlo por la fuente que desees)
+                  height: 1.5, // Espaciado entre líneas
+                ),
+                textAlign: TextAlign.center, // Centrando el texto
+              ),*/
           ],
         ),
       ),
@@ -95,15 +119,18 @@ class HomePage extends StatelessWidget {
           'subject=Portafolio&body=Hola, me gustaría saber más sobre tu portafolio.',
     );
 
+    print('Intentando abrir: $emailUri'); // Imprimir la URI para depuración
+
     if (await canLaunchUrl(emailUri)) {
       await launchUrl(emailUri);
     } else {
+      print('No se pudo enviar el correo: $emailUri');
       throw 'No se pudo enviar el correo';
     }
   }
 
   void _openWhatsApp(BuildContext context) async {
-    final String phoneNumber = '573202499745';
+    final String phoneNumber = '573202499745'; // El número sin "+"
     final String message =
         Uri.encodeComponent('Hola Cristian, me gustaría contactarte.');
     final Uri whatsappUri =
@@ -112,11 +139,12 @@ class HomePage extends StatelessWidget {
     if (await canLaunchUrl(whatsappUri)) {
       await launchUrl(whatsappUri);
     } else {
+      print(
+          'No se pudo abrir WhatsApp. Asegúrate de que esté instalado y que el número sea válido.');
       ScaffoldMessenger.of(context).showSnackBar(
         SnackBar(
-          content: Text(
-              'No se pudo abrir WhatsApp. Asegúrate de que esté instalado y que el número sea válido.'),
-        ),
+            content: Text(
+                'No se pudo abrir WhatsApp. Asegúrate de que esté instalado y que el número sea válido.')),
       );
     }
   }
@@ -125,74 +153,60 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Colors.black,
+        backgroundColor: Colors.blue,
         title: Row(
           children: [
             CircleAvatar(
               backgroundImage: AssetImage('assets/Chris.png'),
               radius: 20,
-              backgroundColor: Colors.white,
             ),
             SizedBox(width: 10),
-            Text(
-              'Cristian Sosa',
-              style: TextStyle(
-                fontWeight: FontWeight.w600,
-                fontSize: 18,
-                letterSpacing: 1.2,
-                color: Colors.white,
-              ),
-            ),
+            Text('Cristian Sosa'),
           ],
         ),
       ),
       body: SingleChildScrollView(
         child: Padding(
-          padding: const EdgeInsets.all(20.0),
+          padding: const EdgeInsets.all(16.0),
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.center,
             children: [
-              SizedBox(height: 20),
               Text(
                 '¡Personal Brand App!',
                 style: TextStyle(
-                  fontSize: 26,
+                  fontSize: 24,
                   fontWeight: FontWeight.bold,
-                  color: Colors.blue[300],
-                  letterSpacing: 1.5,
+                  color: Colors.blue,
                 ),
                 textAlign: TextAlign.center,
               ),
-              SizedBox(height: 25),
+              SizedBox(height: 20),
               CircleAvatar(
                 backgroundImage: AssetImage('assets/Chris.png'),
-                radius: 70,
-                backgroundColor: Colors.transparent,
+                radius: 60,
               ),
-              SizedBox(height: 25),
+              SizedBox(height: 20),
               Text(
-                'Desarrollador de Aplicaciones y Gestor de Desarrollo Comercial, con sólida experiencia en la creación de soluciones innovadoras y en la dirección de estrategias de ventas. Mi enfoque integra habilidades técnicas y comerciales para generar resultados efectivos y duraderos.',
-                style: TextStyle(
-                  fontSize: 16,
-                  fontWeight: FontWeight.w400,
-                  height: 1.5,
-                  color: Colors.white70,
-                ),
+                'Desarrollador de Aplicaciones y Gestor de Desarrollo Comercial,'
+                'con sólida experiencia en la creación de soluciones innovadoras y en la dirección '
+                'de estrategias de ventas. Mi enfoque integra habilidades técnicas y comerciales'
+                'para generar resultados efectivos y duraderos.',
+                style: TextStyle(fontSize: 16),
                 textAlign: TextAlign.justify,
               ),
-              SizedBox(height: 30),
+              SizedBox(height: 20),
               Text(
                 'Mis Habilidades:',
                 style: TextStyle(
-                  fontSize: 22,
+                  fontSize: 20,
                   fontWeight: FontWeight.bold,
-                  color: Colors.blue[300],
+                  color: Colors.blue,
                 ),
               ),
-              SizedBox(height: 20),
+              SizedBox(height: 10),
               Wrap(
-                spacing: 12,
-                runSpacing: 12,
+                spacing: 10,
+                runSpacing: 10,
                 alignment: WrapAlignment.center,
                 children: [
                   _buildSkillCard('Desarrollo Web', Icons.code),
@@ -202,30 +216,25 @@ class HomePage extends StatelessWidget {
                   _buildSkillCard('Ventas', Icons.sell),
                 ],
               ),
-              SizedBox(height: 35),
+              SizedBox(height: 30),
               Row(
                 mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                 children: [
                   ElevatedButton.icon(
                     onPressed: _sendEmail,
-                    icon: Icon(Icons.mail_outline),
+                    icon: Icon(Icons.mail),
                     label: Text('Portafolio'),
                     style: ElevatedButton.styleFrom(
-                      backgroundColor: Colors.blue[800],
-                      padding:
-                          EdgeInsets.symmetric(horizontal: 20, vertical: 15),
-                      textStyle: TextStyle(fontSize: 16),
+                      backgroundColor: const Color.fromARGB(255, 0, 0, 0),
                     ),
                   ),
                   ElevatedButton.icon(
-                    onPressed: () => _openWhatsApp(context),
-                    icon: Icon(Icons.message_outlined),
+                    onPressed: () =>
+                        _openWhatsApp(context), // Pasar el contexto
+                    icon: Icon(Icons.message),
                     label: Text('Contáctame'),
                     style: ElevatedButton.styleFrom(
-                      backgroundColor: Colors.green[800],
-                      padding:
-                          EdgeInsets.symmetric(horizontal: 20, vertical: 15),
-                      textStyle: TextStyle(fontSize: 16),
+                      backgroundColor: const Color.fromARGB(255, 0, 0, 0),
                     ),
                   ),
                 ],
@@ -234,34 +243,22 @@ class HomePage extends StatelessWidget {
           ),
         ),
       ),
-      backgroundColor: Colors.black,
     );
   }
 
   Widget _buildSkillCard(String skill, IconData icon) {
     return Card(
-      color: Colors.grey[900],
-      elevation: 8,
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(15),
-      ),
+      elevation: 4,
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
       child: Padding(
-        padding: const EdgeInsets.all(12.0),
+        padding: const EdgeInsets.all(8.0),
         child: Column(
           children: [
-            Icon(
-              icon,
-              size: 40,
-              color: Colors.blue[300],
-            ),
+            Icon(icon, size: 40, color: Colors.blue),
             SizedBox(height: 10),
             Text(
               skill,
-              style: TextStyle(
-                fontSize: 18,
-                fontWeight: FontWeight.w600,
-                color: Colors.white70,
-              ),
+              style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
             ),
           ],
         ),
